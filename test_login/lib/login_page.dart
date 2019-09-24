00cb96cb3fb064729b998a96feb95833b57c84ea
Reinmar92 <55656055+Reinmar92@users.noreply.github.com>
2019-09-24 16:25:04 +0200

    Google Map Marker
    Marker button added

--- test_login/lib/login_page.dart
+++ test_login/lib/login_page.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class LoginPage  extends StatefulWidget {
-  const LoginPage ({Key key}) : super(key: key);
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key key}) : super(key: key);
 
   @override
   _LoginPageState createState() => _LoginPageState();
@@ -12,24 +12,46 @@ class LoginPage  extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  static const LatLng _center = const LatLng(45.521563, -122.677433);
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
   }
-  
- //sms auth controlling
-  String uid='';
-  getUid(){}
+
+  final Set<Marker> _markers = {};
+
+  LatLng _lastPosition = _center;
+
+  void _addMarkerButton() {
+    setState(() {
+      _markers.add(Marker(
+        markerId: MarkerId(_lastPosition.toString()),
+        position: _lastPosition,
+        infoWindow: InfoWindow(
+          title: 'Użytkownik',
+          snippet: uid,
+        ),
+        icon: BitmapDescriptor.defaultMarker,
+      ));
+    });
+  }
+
+  void _onCameraMove(CameraPosition position) {
+    _lastPosition = position.target;
+  }
+
+  //sms auth controlling
+  String uid = '';
+  getUid() {}
 
   @override
-  void initState(){
-    this.uid='';
-    FirebaseAuth.instance.currentUser().then((val){
+  void initState() {
+    this.uid = '';
+    FirebaseAuth.instance.currentUser().then((val) {
       setState(() {
-       this.uid=val.uid; 
+        this.uid = val.uid;
       });
-    }).catchError((e){
+    }).catchError((e) {
       print(e);
     });
     super.initState();
@@ -39,42 +61,51 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text ("Login Map Page"),
+        title: new Text("Login Map Page"),
         actions: <Widget>[
           FlatButton(
             color: Colors.blue,
             textColor: Colors.white,
-                child: Text('Wyloguj', style: TextStyle(fontSize: 20),),
-                onPressed: (){
-                  FirebaseAuth.instance.signOut().then((action){
-                    Navigator.of(context).pushReplacementNamed('/landingpage');
-                  }).catchError((e){
-                    print(e);
-                  });
-                },
-              ),
-
+            child: Text(
+              'Wyloguj',
+              style: TextStyle(fontSize: 20),
+            ),
+            onPressed: () {
+              FirebaseAuth.instance.signOut().then((action) {
+                Navigator.of(context).pushReplacementNamed('/landingpage');
+              }).catchError((e) {
+                print(e);
+              });
+            },
+          ),
         ],
-        
       ),
-      body: Center(
-        child: Container(
-          child: new Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              new Text('Zalogowano użytkownika $uid'),
-              SizedBox(height: 15.0,
-              ),
-              GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: CameraPosition(
-            target: _center,
-            zoom: 11.0,
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+            onMapCreated: _onMapCreated,
+            initialCameraPosition: CameraPosition(
+              target: _center,
+              zoom: 11.0,
+            ),
+            markers: _markers,
+            onCameraMove: _onCameraMove,
           ),
-              ),
-            ],
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Align(
+              alignment: Alignment.topRight,
+              child: Column(children: <Widget>[
+                FloatingActionButton(
+                  onPressed: _addMarkerButton,
+                  materialTapTargetSize: MaterialTapTargetSize.padded,
+                  backgroundColor: Colors.blue,
+                  child: const Icon(Icons.add_location, size: 35.0),)
+              ],),
+            ),
           ),
-        ),),
+        ],
+      ),
     );
   }
 }
