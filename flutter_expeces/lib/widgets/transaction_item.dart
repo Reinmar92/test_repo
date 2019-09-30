554ab5694ff6ed25d4600f218550ac5c586c8631
Reinmar92 <55656055+Reinmar92@users.noreply.github.com>
2019-09-30 16:43:31 +0200

    Refactor
    changing TransactionItem StatelessWidget to Stateful

--- flutter_expeces/lib/widgets/transaction_item.dart
+++ flutter_expeces/lib/widgets/transaction_item.dart
@@ -1,8 +1,10 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_expeces/models/transaction.dart';
 import 'package:intl/intl.dart';
 
-class TransactionItem extends StatelessWidget {
+class TransactionItem extends StatefulWidget {
   const TransactionItem({
     Key key,
     @required this.transaction,
@@ -13,6 +15,24 @@ class TransactionItem extends StatelessWidget {
   final Function deleteTx;
 
   @override
+  _TransactionItemState createState() => _TransactionItemState();
+}
+
+class _TransactionItemState extends State<TransactionItem> {
+  Color _bgColor;
+  @override
+  void initState() {
+    const avaibleColors = [
+      Colors.red,
+      Colors.black,
+      Colors.blue,
+      Colors.lime
+      ];
+
+    _bgColor=avaibleColors[Random().nextInt(4)];
+    super.initState();
+  }
+  @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 5,
@@ -22,31 +42,32 @@ class TransactionItem extends StatelessWidget {
       ),
       child: ListTile(
         leading: CircleAvatar(
+          backgroundColor: _bgColor,
           radius: 30,
           child: Padding(
             padding: EdgeInsets.all(6),
             child: FittedBox(
-                child: Text('\$${transaction.amount}')),
+                child: Text('\$${widget.transaction.amount}')),
           ),
         ),
         title: Text(
-          transaction.title,
+          widget.transaction.title,
           style: Theme.of(context).textTheme.title,
         ),
         subtitle: Text(
-          DateFormat.yMMMMd().format(transaction.date),
+          DateFormat.yMMMMd().format(widget.transaction.date),
         ),
         trailing: MediaQuery.of(context).size.width>460 
         ? FlatButton.icon(
           icon: Icon(Icons.delete),
           label: Text('Delete'),
           textColor: Theme.of(context).errorColor,
-          onPressed: () => deleteTx(transaction.id),
+          onPressed: () => widget.deleteTx(widget.transaction.id),
           )
           : IconButton(
           icon: Icon(Icons.delete),
           color: Theme.of(context).errorColor,
-          onPressed: () => deleteTx(transaction.id),
+          onPressed: () => widget.deleteTx(widget.transaction.id),
         ),
       ),
     );
